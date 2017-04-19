f9423ecb5bbcdb80676884dca67714467070032d
estebanrfp <estebanrfp@gmail.com>
2017-04-19 05:36:48 +0200

    update project

--- auto.js
+++ auto.js
@@ -1,6 +1,6 @@
 var cron = require('node-cron');
 const git = require('simple-git');
-const git = require('firebase');
+const firebase = require('firebase');
 //const path = __dirname + '/iotos';
 
 cron.schedule('*/1 * * * *', function(){
