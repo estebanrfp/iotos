f7f0f5db8767d0f3c92edae919a024522d19f0be
estebanrfp <estebanrfp@gmail.com>
2017-04-19 07:53:35 +0200

    update project

--- auto.js
+++ auto.js
@@ -1,12 +1,25 @@
-var cron = require('node-cron');
+// var cron = require('node-cron');
 const git = require('simple-git');
-// const firebase = require('firebase');
+const firebase = require('firebase')
+const config = require('./config')
+
 // const path = __dirname + '/iotos';
 // git(path).then etc..
 
+firebase.initializeApp(config);
 
-var task = cron.schedule('*/1 * * * *', function() {
-  console.log('running a task every minute');
+var ref = firebase.database().ref().child("servers")
+
+ref.on('value', function (data) {
+	if (data.val().rpi == "pull") {
+		deploy();
+	} else {
+		console.log('normal')
+	}
+})
+
+function deploy () {
+  console.log('Starting Deploying aplication ... :-)');
   // git(path).pull('origin', 'master')
 	git()
 	.then(function() {
@@ -14,7 +27,6 @@ var task = cron.schedule('*/1 * * * *', function() {
 	})
 	.pull(function(err, update) {
 		if(update && update.summary.changes) {
-			// task.stop();
 			console.log('processing and restarting app...');
 			require('child_process').exec('npm restart app');
 		}
@@ -22,4 +34,10 @@ var task = cron.schedule('*/1 * * * *', function() {
 	.then(function() {
 	console.log('pull done.');
 	});
-});
+}
+
+// var task = cron.schedule('*/1 * * * *', function() {
+//   console.log('running a task every minute');
+//   deploy ()
+//   // task.stop();
+// });
--- deploy.js
+++ deploy.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+// const firebase = require('firebase')
+// const config = require('./config')
+
+// firebase.initializeApp(config);
+
+// var ref = firebase.database().ref().child("servers")
+
+// ref.on('value', function (data) {
+//     if (data.val().rpi == "rpi") {
+//         console.log('on :-)')
+//     } else {
+//         console.log('normal')
+//     }
+// })
