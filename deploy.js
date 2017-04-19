f7ced59282dd3b40238cd88109c45705de740628
estebanrfp <estebanrfp@gmail.com>
2017-04-19 08:20:54 +0200

    update projecct

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const config = require('./config')
 
 firebase.initializeApp(config);
 
-var ref = firebase.database().ref().child("servers").child("rpi1");
+var ref = firebase.database().ref().child("servers");
 
 //ref.rpi1.set("pull");
-ref.set("pull");
+ref.child("rpi1").set("pull");
