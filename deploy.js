e0c45ad12e43e37a92a0799f51a7b01e7a105ebd
estebanrfp <estebanrfp@gmail.com>
2017-04-19 08:21:41 +0200

    update project

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const config = require('./config')
 
 firebase.initializeApp(config);
 
-var ref = firebase.database().ref().child("servers");
+var ref = firebase.database().ref().child("servers").child("rpi1");
 
 //ref.rpi1.set("pull");
-ref.child("rpi1").set("pull");
+ref.set({rpi1: "pull"});
