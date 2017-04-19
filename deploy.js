dd7e364d4486bf29073461c8af8574ca754f8760
estebanrfp <estebanrfp@gmail.com>
2017-04-19 08:06:35 +0200

    update project

--- deploy.js
+++ deploy.js
@@ -1,14 +1,8 @@
-// const firebase = require('firebase')
-// const config = require('./config')
+const firebase = require('firebase')
+const config = require('./config')
 
-// firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-// var ref = firebase.database().ref().child("servers")
+var ref = firebase.database().ref().child("servers");
 
-// ref.on('value', function (data) {
-//     if (data.val().rpi == "rpi") {
-//         console.log('on :-)')
-//     } else {
-//         console.log('normal')
-//     }
-// })
+ref.rpi1.set("pull");
