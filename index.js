9dde1cba4afb4019e385c0f307580e8aaaa1a79c
estebanrfp <estebanrfp@gmail.com>
2017-04-19 10:17:31 +0200

    update context

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var led = new Gpio(17, 'out');
 
 ref.on('value', function (data) {
     if (data.val().Light == "1") {
-        console.log('on :-)')
-        led.writeSync(1);
+        console.log('on :-) ahora si')
+        // led.writeSync(1);
 
-        // var iv = setInterval(function(){
-        //     led.writeSync(led.readSync() === 0 ? 1 : 0)
-        // }, 500);
+        var iv = setInterval(function(){
+            led.writeSync(led.readSync() === 0 ? 1 : 0)
+        }, 500);
 
         // var iv = setInterval(function(){
         //     led.writeSync(led.readSync() === 0 ? 1 : 0)
