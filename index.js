653810c418bba216ddf12af7ef06a6a1276d8ec3
estebanrfp <estebanrfp@gmail.com>
2017-04-19 06:42:13 +0200

    update project

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var led = new Gpio(17, 'out');
 
 ref.on('value', function (data) {
     if (data.val().Light == "1") {
-        console.log('on')
+        console.log('on :-)')
         led.writeSync(1);
     } else {
         console.log('off')
