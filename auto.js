c1e75ae3a4d7ecf16f2817346ad12e6855d10cb2
estebanrfp <estebanrfp@gmail.com>
2017-04-22 14:39:31 +0200

    update project

--- auto.js
+++ auto.js
@@ -13,18 +13,18 @@ ref.child(config.device).on('value', deploy)
 
 function deploy (data) {
 	// console.log(data.val())
-  	// require('child_process').exec(`docker build -t estebanrfp/iotos:latest https://github.com/estebanrfp/iotos.git`)
-	// require('child_process').exec(`docker run --privileged -e DEVICE='${config.device}' -e APIKEY='${config.apiKey}' -e AUTHDOMAIN='${config.authDomain}' -e DATABASEURL='${config.databaseURL}' estebanrfp/iotos`);
-	git()
-	.then(function() {
-		console.log('Starting pull ... ');
-	})
-	.pull(function(err, update) {
-		if(update && update.summary.changes) {
-			console.log('processing and restarting app ...');
-		}
-	})
-	.then(function() {
-		console.log('pull done. :-)');
-	});
+  	require('child_process').exec(`docker build -t estebanrfp/iotos:latest https://github.com/estebanrfp/iotos.git`)
+	require('child_process').exec(`docker run --privileged -e DEVICE='${config.device}' -e APIKEY='${config.apiKey}' -e AUTHDOMAIN='${config.authDomain}' -e DATABASEURL='${config.databaseURL}' estebanrfp/iotos`);
+	// git()
+	// .then(function() {
+	// 	console.log('Starting pull ... ');
+	// })
+	// .pull(function(err, update) {
+	// 	if(update && update.summary.changes) {
+	// 		console.log('processing and restarting app ...');
+	// 	}
+	// })
+	// .then(function() {
+	// 	console.log('pull done. :-)');
+	// });
 }
