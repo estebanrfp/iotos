049c6f10ec29685d022dab3216124867d5056b91
estebanrfp <estebanrfp@gmail.com>
2017-04-22 13:09:42 +0200

    added envieronment variables

--- auto.js
+++ auto.js
@@ -19,7 +19,7 @@ ref.child(config.device).on('value', deploy)
 function deploy (data) {
   console.log('Starting Deploying aplication ...');
   	require('child_process').exec(`docker build -t estebanrfp/iotos:latest https://github.com/estebanrfp/iotos.git`)
-	require('child_process').exec(`docker run --privileged -e DEVICE='${DEVICE}' -e APIKEY='${APIKEY}' -e AUTHDOMAIN='${AUTHDOMAIN}' -e DATABASEURL='${DATABASEURL}' estebanrfp/iotos`);
+	require('child_process').exec(`docker run --privileged -e DEVICE='${config.device}' -e APIKEY='${config.apiKey}' -e AUTHDOMAIN='${config.authDomain}' -e DATABASEURL='${config.databaseURL}' estebanrfp/iotos`);
 	// git()
 	// .then(function() {
 	// 	console.log('Starting pull ... ');
