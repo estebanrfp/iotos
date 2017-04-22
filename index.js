6a5451c618e75ad3a9e97c63b4b6b7f48b84af28
estebanrfp <estebanrfp@gmail.com>
2017-04-22 14:47:22 +0200

    update project

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ ref.on('value', function (data) {
     if (data.val().Light == "1") {
         console.log('on')
         // led.writeSync(1);
-        // var iv = setInterval(function(){
-        //     led.writeSync(led.readSync() === 0 ? 1 : 0)
-        // }, 500);
         var iv = setInterval(function(){
             led.writeSync(led.readSync() === 0 ? 1 : 0)
-        }, 100);
+        }, 800);
+        // var iv = setInterval(function(){
+        //     led.writeSync(led.readSync() === 0 ? 1 : 0)
+        // }, 100);
     } else {
         console.log('off')
         led.writeSync(0);
