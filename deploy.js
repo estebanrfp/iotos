0f6fd5bf81795c49a40f83dfda9eb2ce59bb983c
estebanrfp <estebanrfp@gmail.com>
2017-04-23 11:35:45 +0200

    update project

--- deploy.js
+++ deploy.js
@@ -1,12 +1,12 @@
 const firebase = require('firebase');
 const config = require('./config');
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
-var ref = firebase.database().ref().child("servers");
+// var ref = firebase.database().ref().child("servers");
 
-ref.child(config.device).set(new Date().getTime());
+// ref.child(config.device).set(new Date().getTime());
 
-ref.once('value', function (snapshot) {
-    process.exit();
-});
+// ref.once('value', function (snapshot) {
+//     process.exit();
+// });
