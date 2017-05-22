aeb76fc0705041c49523fcf4377de82916b21d3a
estebanrfp <estebanrfp@gmail.com>
2017-05-22 17:58:11 +0200

    update file

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ ref.on('value', function (data) {
     if (data.val().Light == "1") {
         console.log('led on :-)')
         led.writeSync(1);
+        
         // var iv = setInterval(function(){
         //     led.writeSync(led.readSync() === 0 ? 1 : 0)
         // }, 800)
