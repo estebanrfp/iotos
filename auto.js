ce20da5b68bebb92271b240bc93682c83ffcb90e
estebanrfp <estebanrfp@gmail.com>
2017-05-22 17:52:29 +0200

    update projecct

--- auto.js
+++ auto.js
@@ -34,8 +34,6 @@ var running = false
 function autoPull (data) {
   // require('child_process').exec(`npm run start`)
   // console.log(data.val())
-  // require('child_process').exec(`docker build -t estebanrfp/iotos:latest https://github.com/estebanrfp/iotos.git`)
-  // require('child_process').exec(`docker run --privileged -e DEVICE='${config.device}' -e APIKEY='${config.apiKey}' -e AUTHDOMAIN='${config.authDomain}' -e DATABASEURL='${config.databaseURL}' estebanrfp/iotos`);
   git()
     .exec(function() {
       console.log('Starting pull ...')
