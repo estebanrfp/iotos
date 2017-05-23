095480644a84ca54292de654527b53de8345f777
estebanrfp <estebanrfp@gmail.com>
2017-05-23 21:28:26 +0200

    testing

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ var led = new Gpio(17, 'out')
 fb.child("room1").on('value', function (data) {
     if (data.val().Light == "1") {
         console.log('led on :-)')
-        // led.writeSync(1)
+        led.writeSync(1)
         // var iv = setInterval(function(){
         //     led.writeSync(led.readSync() === 0 ? 1 : 0)
         // }, 800)
-        var iv = setInterval(function(){
-            led.writeSync(led.readSync() === 0 ? 1 : 0)
-        }, 100)
+        // var iv = setInterval(function(){
+        //     led.writeSync(led.readSync() === 0 ? 1 : 0)
+        // }, 100)
     } else {
         console.log('led off')
         led.writeSync(0)
