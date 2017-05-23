441a33a270f46c1265643cef3219eaa03f7e0ed6
estebanrfp <estebanrfp@gmail.com>
2017-05-24 00:50:44 +0200

    update process

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var led = new Gpio(17, 'out')
 
 fb.child("room1").on('value', function (data) {
     if (data.val().Light == "1") {
-        console.log('led on :-) ahora si')
+        console.log('led on :-)')
         led.writeSync(1)
         // var iv = setInterval(function(){
         //     led.writeSync(led.readSync() === 0 ? 1 : 0)
