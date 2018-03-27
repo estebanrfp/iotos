99edcdbb0f11c001b7ef8c3620aa79673bcd6df3
estebanrfp <estebanrfp@gmail.com>
2018-03-27 13:55:36 +0200

    update project

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function light (room, gpio) {
 
   fb.child(room).on('value', function (data) {
     if (data.val().Light == "1") {
-      led.writeSync(1)
-      // var iv = setInterval(function(){
-      //     led.writeSync(led.readSync() === 0 ? 1 : 0)
-      // }, 800)
+      // led.writeSync(1)
+      var iv = setInterval(function(){
+          led.writeSync(led.readSync() === 0 ? 1 : 0)
+      }, 800)
       // var iv = setInterval(function(){
       //     led.writeSync(led.readSync() === 0 ? 1 : 0)
       // }, 100)
